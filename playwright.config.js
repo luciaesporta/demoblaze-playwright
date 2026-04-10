ee52035ed6078ac5924110e928beb9dc7d5004e7
Lucia Esporta <luciaalvarezesporta@gmail.com>
2026-04-10 10:26:14 -0300

    ci: add html reporter and upgrade Node.js to 22

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
+  reporter: [['html', { open: 'never' }]],
   use: {
     baseURL: 'https://www.demoblaze.com',
     headless: true,
