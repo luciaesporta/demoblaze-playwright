9d4cfd1eaea85a87309f2df7963e187651c05d5a
Lucia Esporta <luciaalvarezesporta@gmail.com>
2026-04-22 00:51:07 -0300

    feat(fixtures): add cartWithOneProduct fixture

--- fixtures/authFixtures.js
+++ fixtures/authFixtures.js
@@ -1,5 +1,7 @@
 const { test: base } = require('@playwright/test');
 const { AuthPage } = require('../pages/AuthPage');
+const { HomePage } = require('../pages/HomePage');
+const { ProductPage } = require('../pages/ProductPage');
 const { generateUser } = require('../utils/userData');
 
 const test = base.extend({
@@ -11,6 +13,22 @@ const test = base.extend({
     await authPage.login(username, password);
 
     await use({ page, username });
+  },
+
+  cartWithOneProduct: async ({ page }, use) => {
+    const authPage = new AuthPage(page);
+    const homePage = new HomePage(page);
+    const productPage = new ProductPage(page);
+    const { username, password } = generateUser();
+
+    await authPage.register(username, password);
+    await authPage.login(username, password);
+
+    await homePage.goto();
+    await homePage.openFirstProduct();
+    const { name, price } = await productPage.addToCartAndCapture();
+
+    await use({ page, name, price });
   },
 });
 
